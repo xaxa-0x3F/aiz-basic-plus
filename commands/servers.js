00cb3ac2d542c05e480cee9be9b8650b20523f4f
eh asuna <bartheimsness2@gmail.com>
2020-12-08 17:17:28 -0600

    m

--- commands/servers.js
+++ commands/servers.js
@@ -2,8 +2,10 @@ module.exports = {
     name: 'servers',
     description: "this displays how many servers AisBasic+ is in! :)",
     execute(message, args, client){
-        client.guilds.cache.forEach((guild) =>{
-            message.channel.send(`${client.guilds.size} servers!`);
-        });
+        var discordservers = [];
+        client.guilds.forEach(g => {
+        discordservers.push(g.name); // use g.id for each server's id, or you can use g.name -- id: g.id
+        })
+        message.channel.send(`${discordservers.join('\n')}`);
     }
 }
