abc5f71fb267f507a08f06db8705121b52ce358f
eh asuna <bartheimsness2@gmail.com>
2020-12-08 14:34:20 -0600

    k

--- command.js
+++ command.js
@@ -1,4 +1,4 @@
-const { prefix } = require('./config.json');
+const { prefix } = '+';
 
 module.exports = (client, aliases, callback) => {
     if (typeof aliases === 'string'){
@@ -9,7 +9,7 @@ module.exports = (client, aliases, callback) => {
 
         aliases.forEach((alias) => {
             const command = `${prefix}${alias}`
-            
+
             if (content.startsWith(`${command}`) || content === command){
                 console.log(`Running the command ${command}`)
                 callback(message)
