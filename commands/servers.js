6e02b037618a928324432d2136dc665d7905e098
eh asuna <bartheimsness2@gmail.com>
2020-12-08 17:15:34 -0600

    mmmm

--- commands/servers.js
+++ commands/servers.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'servers',
     description: "this displays how many servers AisBasic+ is in! :)",
-    execute(message, args, client, guilds){
+    execute(message, args, client){
         client.guilds.cache.forEach((guild) =>{
-            message.channel.send(guild);
+            message.channel.send(`${client.guilds.size} servers!`);
         });
     }
 }
