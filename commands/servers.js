03e3d21d3190e0ca0e662985029e6847d590635b
eh asuna <bartheimsness2@gmail.com>
2020-12-08 17:03:21 -0600

    fixServer

--- commands/servers.js
+++ commands/servers.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'servers',
     description: "this displays how many servers AisBasic+ is in! :)",
-    execute(message, args){
+    execute(message, args, client){
         client.guilds.cache.forEach((guild) =>{
             message.channel.send(guild);
         });
