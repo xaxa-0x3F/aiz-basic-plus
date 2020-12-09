e8608aeb57a8ec638092c250a5329db204f2cc07
eh asuna <bartheimsness2@gmail.com>
2020-12-08 20:31:42 -0600

    added ping update

--- commands/ping.js
+++ commands/ping.js
@@ -12,6 +12,9 @@ module.exports = {
         .setTitle('PONG!')
         .setURL('https://www.youtube.com/channel/UC7Rlc68ImIV32Y95xLmTSpA?view_as=subscribera')
         .setDescription('This is a link to invite me to your server!')
+        .addFields(
+            {name: 'pong', value: 'pong'}
+        )
         .setImage('https://thumbs.gfycat.com/DaringDimwittedFlyingsquirrel-max-1mb.gif')
         .setFooter('hehheheheheh<3');
 
