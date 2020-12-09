2e9fdffa5bebb09c90b35389d0eb458e48feb1b5
eh asuna <bartheimsness2@gmail.com>
2020-12-08 23:30:45 -0600

    rrsucksass

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -16,8 +16,10 @@ module.exports = {
 
         client.on('messageReactionAdd', async (reaction, user) => {
             if(reaction.message.partial) await reaction.message.fetch();
+            if(reaction.partial) await reaction.fetch();
             if(user.bot) return;
             if(!reaction.message.guild) return;
+            
             if(!reaction.message.channel.id == channel){
                 if(reaction.emoji.name === memberEmoji){
                     await reaction.message.guild.members.cache.get(user.id).roles.add(memberRole);
