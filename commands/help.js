0abc5b34bed4604c09686e09999195f8ef9ba589
eh asuna <bartheimsness2@gmail.com>
2020-12-09 01:22:49 -0600

    plx wrx

--- commands/help.js
+++ commands/help.js
@@ -10,21 +10,14 @@ module.exports = {
         .setURL('https://www.youtube.com/channel/UC7Rlc68ImIV32Y95xLmTSpA?view_as=subscribera')
         .setDescription('Moderation commands can only be performed by authorized roles.')
         .addFields(
-            //{name: 'ping', value: 'reutrns: "pong!"'},
-            //{name: 'youtube', value: 'sends a link to the cretor`s youtube.'},
-            //{name: 'dungeon destiny', value: 'sends a link to Dungeon Destiny.'},
-            //{name: 'makeabot', value: 'sends a youtube link on where to learn how to make a bot'},
             {name: 'timedmute', value: 'mutes a user used as~ +timedmute @user 1m ~ (1s, 1m, 1h, 1d, 1y'},
             {name: 'mute', value: 'mutes a user used as~ +mute @user'},
             {name: 'unmute', value: 'unmutes a user used as~ +unmute @user'},
             {name: 'kick', value: 'kicks a member used as~ +kick @user'},
             {name: 'ban', value: 'bans a member used as~ +ban @user'},
             {name: 'delete', value: 'deletes the amount of messages entered used as~ +delete 20'}
-            //{name: 'inviteme', value: 'sends a link to invite me to your server!'},
-            //{name: 'servers', value: 'displays how many servers Im currently in!'}
         )
         //.setImage('https://pa1.narvii.com/6518/5354d7ecc11e93741984c1ca103f37b5eecca047_hq.gif')
-        .setTimeStamp()
         .setFooter('More help here~>\nhttps://discord.gg/bR4R4PE6a4');
         
         const fun = Discord.MessageEmbed()
@@ -41,7 +34,6 @@ module.exports = {
             {name: 'servers', value: 'displays how many servers Im currently in!'}
         )
         //.setImage('https://pa1.narvii.com/6518/5354d7ecc11e93741984c1ca103f37b5eecca047_hq.gif')
-        .setTimeStamp()
         .setFooter('More help here~>\nhttps://discord.gg/bR4R4PE6a4');
         
         const pages = [
