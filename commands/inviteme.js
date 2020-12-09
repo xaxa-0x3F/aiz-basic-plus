2935bc6d0d06c28c93293e7697a404e5ebc8537d
eh asuna <bartheimsness2@gmail.com>
2020-12-08 20:29:17 -0600

    mm

--- commands/inviteme.js
+++ commands/inviteme.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#FFB6C1')
         .setTitle('Invite me here!')
         .setURL('https://www.youtube.com/channel/UC7Rlc68ImIV32Y95xLmTSpA?view_as=subscribera')
-        .setDescription('This is an embed for help')
+        .setDescription('This is a link to invite me to your server!')
         .addFields(
             {name: 'invite', value: 'https://discord.com/api/oauth2/authorize?client_id=784994557489184779&permissions=8&scope=bot'}
         )
--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,21 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command",
     execute(message, args){
-        message.channel.send('pong!');
+        const Discord = require('discord.js');
+module.exports = {
+    name: 'inviteme',
+    description: "How to invite.",
+    execute(message, args){
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#FFB6C1')
+        .setTitle('PONG!')
+        .setURL('https://www.youtube.com/channel/UC7Rlc68ImIV32Y95xLmTSpA?view_as=subscribera')
+        .setDescription('This is a link to invite me to your server!')
+        .setImage('https://thumbs.gfycat.com/DaringDimwittedFlyingsquirrel-max-1mb.gif')
+        .setFooter('hehheheheheh<3');
+
+        message.channel.send(newEmbed);
+    }
+}
     }
 }
