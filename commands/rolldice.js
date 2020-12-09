9b2ae7b8163a898791a8a9e0f519f87828b10be0
eh asuna <bartheimsness2@gmail.com>
2020-12-09 14:19:14 -0600

    mute all

--- commands/rolldice.js
+++ commands/rolldice.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'rolldice',
     description: "this is a command that rolls a die and outputs the number.",
     execute(message, args){
-        message.channel.send(`The dice rolled ${Math.floor((Math.random() * 6) + 1)};
-        }!`);
+        message.channel.send(`The dice rolled ${Math.floor((Math.random() * 6) + 1)}!`);
     }
 }
