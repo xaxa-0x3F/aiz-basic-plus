b2bcf91e0dbe0597fcdc8a74eaeb011c967472ca
eh asuna <bartheimsness2@gmail.com>
2020-12-10 02:35:27 -0600

    m

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
 const command = require('./command');
-const prefix = '+';
+const db = require('quick.db');
+const prefix = db.fetch(`${message.guild.id}prefix`);
+if(prefix === null || isNaN(prefix)){
+  prefix = '+'
+}
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 var discordservers = [];
 
@@ -54,8 +58,11 @@ try{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-    if(command === 'ping'){
+    if(command === 'prefix'){
+        db.set(`${message.guild.id}prefix`, args[0])
+        return message.reply(`your prefix is now ${args[0]}`) 
+    }
+     else if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     } else if(command == 'youtube'){
         client.commands.get('youtube').execute(message, args);
@@ -96,15 +103,7 @@ try{
         for (let member of channel.members) {
             member[1].setMute(true)
         }
-     } else if (message.content == 'prefix'){
-        if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You are not allowed to change the prefix');
-        if(!args[1]) return message.channel.send('You need to specify a prefix.');
-        if(args[1].length > 3) return message.channel.send('A prefix can only be 3 or less characters');
-        if(args[1] === db.get(`guild_${message.guild.id}_prefix`)) return message.channel.send('That is already your prefix');
-        if(args[1] === "?") db.delete(`guild_${message.guild.id}_prefix`);
-        db.set(`guild_${message.guild.id}_prefix`, args[1]);
-        return message.channel.send(`Your new prefix is ${args[1]}`); 
-    }
+     }
 } catch (err){
     message.channel.send('Invalid or incomplete command. Try `+help` for more info.\n`' + err + '`');
 } 
