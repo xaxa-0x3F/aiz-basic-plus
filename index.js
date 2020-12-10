38ea6419005591eee2ac2ddf54e71a1f488e1b35
eh asuna <bartheimsness2@gmail.com>
2020-12-09 19:29:16 -0600

    PLEASE

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('ready', ()=> {
 
 client.on('message', async message =>{
 try{
-    const prefix =db.get(`guild_${message.guild.id}_prefix` || "?");
+    const prefixx =db.get(`guild_${message.guild.id}_prefix` || "?");
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -102,9 +102,9 @@ try{
         if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You are not allowed to change the prefix');
         if(!args[1]) return message.channel.send('You need to specify a prefix.');
         if(args[1].length > 3) return message.channel.send('A prefixx can only be 3 or less characters');
-        if(args[1] === db.get(`guild_${message.guild.id}_prefix`)) return message.channel.send('That is already your prefix');
-        if(args[1] === "?") db.delete(`guild_${message.guild.id}_prefix`);
-        db.set(`guild_${message.guild.id}_prefix`, args[1]);
+        if(args[1] === db.get(`guild_${message.guild.id}_prefixx`)) return message.channel.send('That is already your prefix');
+        if(args[1] === "?") db.delete(`guild_${message.guild.id}_prefixx`);
+        db.set(`guild_${message.guild.id}_prefixx`, args[1]);
         return message.channel.send(`Your new prefix is ${args[1]}`); 
      } 
 } catch (err){
