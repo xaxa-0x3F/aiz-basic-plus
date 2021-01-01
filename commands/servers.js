2fb6981114ba780f9f93d76af6fc8194e3b23642
eh asuna <bartheimsness2@gmail.com>
2021-01-01 16:12:13 -0600

    m

--- commands/servers.js
+++ commands/servers.js
@@ -6,7 +6,6 @@ module.exports = {
     execute(message, client, args){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#FFB6C1')
-        .setTitle('Thanks for considering voting!')
         .setDescription(`I'm in ${client.guilds.cache.size} servers!`);
 
         message.channel.send(newEmbed);
