0b0ea72c79e422f59e117a70c1d7319daf7118e9
eh asuna <bartheimsness2@gmail.com>
2021-01-01 21:24:15 -0600

    m

--- commands/play.js
+++ commands/play.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message, args){
         const voiceChannel = message.member.voice.channel;
 
-        if(!voicechannel) return message.channel.send('You need to be in a voice channel to use this command ❤')
+        if(!voiceChannel) return message.channel.send('You need to be in a voice channel to use this command ❤')
         const permissions = voiceChannel.permissionsFor(message.client.user);
         if(!permissions.has('CONNECT')) return message.channel.send('You are not allowed to use this command ❤')
         if(!permissions.has('SPEAK')) return message.channel.send('You are not allowed to use this command ❤')
